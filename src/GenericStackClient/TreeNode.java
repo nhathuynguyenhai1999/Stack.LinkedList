abf1708c9da7e2695656a74b73f1806b897171f2
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-01-30 16:48:35 +0700

    update my coding files

--- src/GenericStackClient/TreeNode.java
+++ src/GenericStackClient/TreeNode.java
@@ -33,6 +33,15 @@ public class TreeNode {
 
             // Bước 3: Truy cập nút gốc
             System.out.print(node.data + " ");
+
+            // Bước 1: Truy cập nút gốc
+            System.out.print(node.data + " ");
+
+            // Bước 2: Duyệt cây con bên trái đệ qui
+            traverseTree(node.left);
+
+            // Bước 3: Duyệt cây con bên phải đệ qui
+            traverseTree(node.right);
         }
     }
     public static void main(String[] args) {
@@ -45,6 +54,18 @@ public class TreeNode {
         tree.root.left.left = new TreeNode(4);
         tree.root.left.right = new TreeNode(5);
         tree.root.right.left = new TreeNode(6);
+        tree.root.right.right = new TreeNode(7);
+
+        System.out.println("Duyệt cây theo thuật toán đã mô tả:");
+        tree.traverseTree(tree.root);
+
+        // Tạo cây ví dụ
+        tree.root = new TreeNode(4);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(6);
+        tree.root.left.left = new TreeNode(1);
+        tree.root.left.right = new TreeNode(3);
+        tree.root.right.left = new TreeNode(5);
         tree.root.right.right = new TreeNode(7);
 
         System.out.println("Duyệt cây theo thuật toán đã mô tả:");
